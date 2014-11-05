49185251c58a4061ccc109235beac7a31e092641
Emory Griffith <emorygriffith91@gmail.com>
2014-11-05 16:30:54 -0500

    started 2nd view

--- app/scripts/views/list_guitar_view.js
+++ app/scripts/views/list_guitar_view.js
@@ -9,9 +9,31 @@
 
     template: _.template($('#listTemp').html()),
 
-    initialize: function() {}
+    initialize: function() {
+
+      this.render();
+
+      this.collection.on('sync', this.render, this);
 
 
+      // Get our Element On Our Page
+      $('#guitarList').html(this.$el);
+
+
+    },
+
+    render: function () {
+        var self = this;
+
+        // Empty out
+        this.$el.empty();
+
+        this.collection.each(function (c) {
+          self.$el.append(self.template(c.toJSON()));
+        });
+
+        return this;
+      }
 
 
 
